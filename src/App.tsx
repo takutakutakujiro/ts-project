f01adbe714a855b30f11aec5aaaaae991ed63ad9
takutakutakujiro <ne230232@gmail.com>
2019-05-02 04:00:26 +0900

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
 import React, { FC } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './container/home';
+import Blog from './container/blog';
 
-const App: FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          っっっっっっっっっっっっっっっｔsss
-        </a>
-      </header>
+const App: FC = () => (
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' component={Home} />
+      <Route path='/blog' component={Blog} />
     </div>
-  );
-}
+  </BrowserRouter>
+)
 
 export default App;
---
+++ src/container/blog/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Blog: FC = () => {
+  return(
+    <div>
+      test
+    </div>
+  )
+}
+
+export default Blog;
